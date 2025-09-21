113eea61336501688fa6122c7e94575fb9fbca6e
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2025-09-21 23:43:20 +0200

    Added simple file selector example

--- composeApp/src/jvmMain/kotlin/com/evandhardspace/loon/App.kt
+++ composeApp/src/jvmMain/kotlin/com/evandhardspace/loon/App.kt
@@ -1,49 +1,30 @@
 package com.evandhardspace.loon
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import loon.composeapp.generated.resources.Res
-import loon.composeapp.generated.resources.compose_multiplatform
+import javax.swing.JFileChooser
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+    var selectedDirectory by remember { mutableStateOf<String?>(null) }
+
+    Column {
+        Button(onClick = {
+            val chooser = JFileChooser().apply {
+                fileSelectionMode = JFileChooser.FILES_AND_DIRECTORIES
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            val result = chooser.showOpenDialog(null)
+            if (result == JFileChooser.APPROVE_OPTION) {
+                selectedDirectory = chooser.selectedFile.absolutePath
             }
+        }) {
+            Text("Select Directory")
+        }
+
+        selectedDirectory?.let {
+            Text("Selected directory: $it")
         }
     }
 }
