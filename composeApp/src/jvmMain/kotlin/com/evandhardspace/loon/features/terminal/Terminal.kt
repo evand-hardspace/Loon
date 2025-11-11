0f81a7186f2e6b39627bb71cb5ee9f91b0ea149a
Ivan Kravchenko <ivan.kravchenko217@gmail.com>
2025-11-12 00:28:33 +0100

    Refactored

--- composeApp/src/jvmMain/kotlin/com/evandhardspace/loon/features/terminal/Terminal.kt
+++ composeApp/src/jvmMain/kotlin/com/evandhardspace/loon/features/terminal/Terminal.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
